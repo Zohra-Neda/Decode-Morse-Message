50998a63c11e6cb08208ec13af7fc23c60c1d627
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-23 11:27:35 +0300

    fix linter errors

--- main.rb
+++ main.rb
@@ -16,13 +16,13 @@ def decode_char(char)
 end
 
 def decode_word(string)
-    string.split('  ').map { |word| word.split.map { |char| decode_char(char) }.join }.join(' ')
-  end
-  
-  def decode(string)
-    string.split('   ').map { |word| decode_word(word) }.join(' ')
-  end
-  
-  p decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-  
-  # Decodes to => A BOX FULL OF RUBIES
+  string.split('  ').map { |word| word.split.map { |char| decode_char(char) }.join }.join(' ')
+end
+
+def decode(string)
+  string.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+p decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+
+# Decodes to => A BOX FULL OF RUBIES
